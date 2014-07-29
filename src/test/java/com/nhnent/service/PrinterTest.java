97eb9a794417a8665a8800bbb94f3afa79a94e26
pokbab <xxxtintin@naver.com>
2014-07-30 01:12:45 +0900

    출력부 클래스 생성

---
+++ src/test/java/com/nhnent/service/PrinterTest.java
@@ -0,0 +1,16 @@
+package com.nhnent.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.nhnent.model.GradeType;
+
+
+public class PrinterTest {
+
+	@Test
+	public void testPrint_등급타입을_콘솔에_출력한다() throws Exception {
+		GradeType type = GradeType.A;
+	}
+}
